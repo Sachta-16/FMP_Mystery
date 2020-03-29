ff2c4c5ffad45bc0b0c5650e5283c47d211df1cd
Sachta-16 <scharlie.tamkin@gmail.com>
2020-03-29 17:20:29 +0100

    Begun animation.

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 100;
     public Rigidbody2D rb;
+    public Animator animator;
 
     public void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Speed", Mathf.Abs(v));
 
         Vector3 tempVect = new Vector3(h, v, 0);
         tempVect = tempVect.normalized * speed * Time.deltaTime;
