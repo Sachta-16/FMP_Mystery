c72b2e8821d3acc73315e2158d7232b1ac5a33e3
Sachta-16 <scharlie.tamkin@gmail.com>
2020-04-02 17:06:04 +0100

    Finished animation.

---
+++ New Unity Project/Assets/Scripts/PlayerFlip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlip : MonoBehaviour
+{
+    private SpriteRenderer mySpriteRenderer;
+    private void Awake()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (mySpriteRenderer != null)
+            {
+                mySpriteRenderer.flipX = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            mySpriteRenderer.flipX = false;
+        }
+    }
+}
--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -13,12 +13,44 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        animator.SetFloat("Speed", Mathf.Abs(v));
-
         Vector3 tempVect = new Vector3(h, v, 0);
         tempVect = tempVect.normalized * speed * Time.deltaTime;
         rb.MovePosition(rb.transform.position + tempVect);
 
+        if (Input.GetKey("w"))
+        {
+                animator.SetInteger("Direction", (2));
+        }
+        else if (Input.GetKeyUp("w"))
+        {
+            animator.SetInteger("Direction", (5));
+        }
 
+        if (Input.GetKey("s"))
+        {
+            animator.SetInteger("Direction", (1));
+        }
+        else if (Input.GetKeyUp("s"))
+        {
+            animator.SetInteger("Direction", (0));
+        }
+
+        if (Input.GetKey("d"))
+        {
+            animator.SetInteger("Direction", (3));
+        }
+        else if (Input.GetKeyUp("d"))
+        {
+            animator.SetInteger("Direction", (0));
+        }
+
+        if (Input.GetKey("a"))
+        {
+            animator.SetInteger("Direction", (4));
+        }
+        else if (Input.GetKeyUp("a"))
+        {
+            animator.SetInteger("Direction", (0));
+        }
     }
 }
