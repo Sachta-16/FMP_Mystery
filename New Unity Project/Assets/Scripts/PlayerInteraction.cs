fb55ae0720949abd20e3b08ad65e2d03a3783f11
Sachta-16 <scharlie.tamkin@gmail.com>
2020-04-22 15:03:50 +0100

    Wrote interaction script

---
+++ New Unity Project/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerInteraction : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Help");
+    }
+}
+
